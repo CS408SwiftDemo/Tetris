71ffe7534c6fc9ac2efa8dc4defda36fef69dd3f
Jose Gutierrez <triforcegold@hotmail.com>
2016-07-19 12:34:05 -0700

    Test

---
+++ Swiftris/GameViewController.swift
@@ -0,0 +1,156 @@
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController, TetrisDelegate, UIGestureRecognizerDelegate {
+
+    var scene: GameScene!
+    var tetris:Tetris!
+    var panPointReference:CGPoint?
+    
+    
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var levelLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Configure the view.
+        let skView = view as! SKView
+        skView.multipleTouchEnabled = false
+        
+        // Create and configure the scene.
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        scene.tick = didTick
+        
+        tetris = Tetris()
+        tetris.delegate = self
+        tetris.beginGame()
+        
+        // Present the scene.
+        skView.presentScene(scene)
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+    @IBAction func didTap(sender: UITapGestureRecognizer) {
+        tetris.rotateShape()
+    }
+    
+    @IBAction func didPan(sender: UIPanGestureRecognizer) {
+        let currentPoint = sender.translationInView(self.view)
+        if let originalPoint = panPointReference {
+            if abs(currentPoint.x - originalPoint.x) > (BlockSize * 0.9) {
+                if sender.velocityInView(self.view).x > CGFloat(0) {
+                    tetris.moveShapeRight()
+                    panPointReference = currentPoint
+                } else {
+                    tetris.moveShapeLeft()
+                    panPointReference = currentPoint
+                }
+            }
+        } else if sender.state == .Began {
+            panPointReference = currentPoint
+        }
+    }
+    
+    @IBAction func didSwipe(sender: UISwipeGestureRecognizer) {
+        tetris.dropShape()
+    }
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailByGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        if gestureRecognizer is UISwipeGestureRecognizer {
+            if otherGestureRecognizer is UIPanGestureRecognizer {
+                return true
+            }
+        } else if gestureRecognizer is UIPanGestureRecognizer {
+            if otherGestureRecognizer is UITapGestureRecognizer {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func didTick() {
+        tetris.letShapeFall()
+    }
+    
+    func nextShape() {
+        let newShapes = tetris.newShape()
+        guard let fallingShape = newShapes.fallingShape else {
+            return
+        }
+        self.scene.addPreviewShapeToScene(newShapes.nextShape!) {}
+        self.scene.movePreviewShape(fallingShape) {
+            self.view.userInteractionEnabled = true
+            self.scene.startTicking()
+        }
+    }
+    
+    func gameDidBegin(tetris: Tetris) {
+        levelLabel.text = "\(tetris.level)"
+        scoreLabel.text = "\(tetris.score)"
+        scene.tickLengthMillis = TickLengthLevelOne
+        
+        // The following is false when restarting a new game
+        if tetris.nextShape != nil && tetris.nextShape!.blocks[0].sprite == nil {
+            scene.addPreviewShapeToScene(tetris.nextShape!) {
+                self.nextShape()
+            }
+        } else {
+            nextShape()
+        }
+    }
+    
+    func gameDidEnd(tetris: Tetris) {
+        view.userInteractionEnabled = false
+        scene.stopTicking()
+        scene.playSound("Sounds/gameover.mp3")
+        scene.animateCollapsingLines(tetris.removeAllBlocks(), fallenBlocks: tetris.removeAllBlocks()) {
+            tetris.beginGame()
+        }
+    }
+    
+    func gameDidLevelUp(tetris: Tetris) {
+        levelLabel.text = "\(tetris.level)"
+        if scene.tickLengthMillis >= 100 {
+            scene.tickLengthMillis -= 100
+        } else if scene.tickLengthMillis > 50 {
+            scene.tickLengthMillis -= 50
+        }
+        scene.playSound("Sounds/levelup.mp3")
+    }
+    
+    func gameShapeDidDrop(tetris: Tetris) {
+        scene.stopTicking()
+        scene.redrawShape(tetris.fallingShape!) {
+            tetris.letShapeFall()
+        }
+        scene.playSound("Sounds/drop.mp3")
+    }
+    
+    func gameShapeDidLand(tetris: Tetris) {
+        scene.stopTicking()
+        self.view.userInteractionEnabled = false
+        let removedLines = tetris.removeCompletedLines()
+        if removedLines.linesRemoved.count > 0 {
+            self.scoreLabel.text = "\(tetris.score)"
+            scene.animateCollapsingLines(removedLines.linesRemoved, fallenBlocks:removedLines.fallenBlocks) {
+                self.gameShapeDidLand(tetris)
+            }
+            scene.playSound("Sounds/bomb.mp3")
+        } else {
+            nextShape()
+        }
+    }
+    
+    func gameShapeDidMove(tetris: Tetris) {
+        scene.redrawShape(tetris.fallingShape!) {}
+    }
+}
